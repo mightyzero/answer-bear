fb0057ffe181a8cc2c4c7c1c54cf1fca0136673b
Minh Pham <minh.pham@utexas.edu>
2019-02-19 21:51:15 -0600

    Implemented a method to reply to a comment.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
     return "Hi, I'm Answer Bear!"
+
+
+@app.route("/", methods=['POST'])
+def reply():
+    comment = request.get_json()['comment']
+    resp = "You said: {}. I don't know how to respond to that, I'm just a bear!".format(comment)
+
+    return jsonify(reply=resp), 400
 
 
 if __name__ == "__main__":
