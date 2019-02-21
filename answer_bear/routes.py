5fb806b2380815f1f1ae3f1c7d790add0e85c06c
Minh Pham <minh.pham@utexas.edu>
2019-02-21 17:56:07 -0600

    Added CORS to allow API to be accessed on Heroku.

--- answer_bear/routes.py
+++ answer_bear/routes.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/", methods=['GET'])
+@cross_origin()
 def hello():
     return "Hi, I'm Answer Bear!"
 
 
 @app.route("/", methods=['POST'])
+@cross_origin()
 def reply():
     # comment: str = request.get_json()['comment']
     resp = "I don't know how to respond to that, I'm just a bear!"
 
-    return jsonify(reply=resp), 400
+    return jsonify(reply=resp)
